e0b70383f0e4db4367fdf4a2f96ecb752aee98aa
Almas Yoko_office <Almas almas@yoko.space>
2025-02-19 15:45:01 +0500

    feat: enhance Login and Register components with form controls and error handling

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,44 @@
-import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { Alert, Button, Form, Row, Col, Stack } from 'react-bootstrap';
 
 const Register = () => {
-    return (<>Register</>);
-}
- 
+    return (
+        <>
+            <Form>
+                <Row
+                    style={{
+                        height: '100vh',
+                        justifyContent: 'center',
+                        paddingTop: '15%',
+                    }}>
+                    <Col xs={6}>
+                        <Stack gap={3}>
+                            <h2>Register</h2>
+                            <Form.Control
+                                type='text'
+                                placeholder='Name'
+                            />
+                            <Form.Control
+                                type='email'
+                                placeholder='Email'
+                            />
+                            <Form.Control
+                                type='password'
+                                placeholder='Password'
+                            />
+                            <Button
+                                variant='primary'
+                                type='submit'>
+                                Register
+                            </Button>
+                            <Alert variant='danger'>
+                                <p>An error occured</p>
+                            </Alert>
+                        </Stack>
+                    </Col>
+                </Row>
+            </Form>
+        </>
+    );
+};
+
 export default Register;
--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -101,5 +101,5 @@ const getUsers = async (req, res) => {
 }
 
 module.exports = { registerUser, loginUser, findUser, getUsers };
-//16:25 real time chat app 3 react bootstrap
-//https://www.youtube.com/watch?v=KxexA_axEGA
+//00:20 real time chat app 3 react bootstrap
+//https://www.youtube.com/watch?v=93NcEY59xmU&list=PL63c_Ws9ecIRZ6njHRi3cuCkNSfzqyLBn&index=6&ab_channel=ChaooCharles
