c37e947b6b13336711282e486c3f2793b43cc96c
Almas Yoko_office <Almas almas@yoko.space>
2025-02-21 21:24:07 +0500

    feat: refactor registerUser function for improved readability and add registerInfo dependency

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -17,29 +17,31 @@ export const AuthContextProvider = ({ children }) => {
         setRegisterInfo(info);
     }, []);
 
-    console.log(registerInfo);
+    const registerUser = useCallback(
+        async (e) => {
+            e.preventDefault();
+            setIsRegisterLoading(true);
+            setRegisterError(null);
 
-    const registerUser = useCallback(async (e) => {
-        e.preventDefault();
-        setIsRegisterLoading(true);
-        setRegisterError(null);
+            const response = await postRequest(
+                `${baseUrl}/users/register`,
+                JSON.stringify(registerInfo)
+            );
 
-        console.log(registerInfo);
+            setIsRegisterLoading(false);
 
-        const response = await postRequest(
-            `${baseUrl}/users/register`,
-            JSON.stringify(registerInfo)
-        );
+            if (response.error) {
+                return setRegisterError({
+                    error: true,
+                    message: response.message,
+                });
+            }
 
-        setIsRegisterLoading(false);
-
-        if (response.error) {
-            return setRegisterError({ error: true, message: response.message });
-        }
-
-        localStorage.setItem('User', JSON.stringify(response));
-        setUser(response);
-    }, []);
+            localStorage.setItem('User', JSON.stringify(response));
+            setUser(response);
+        },
+        [registerInfo]
+    );
 
     return (
         <AuthContext.Provider
