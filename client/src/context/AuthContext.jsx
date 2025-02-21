ca05ef2a09f591db4e9fa2990edb889d6641310c
Almas Yoko_office <Almas almas@yoko.space>
2025-02-21 21:19:27 +0500

    feat: enhance registration process with error handling and loading state management

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,24 +1,56 @@
 import { createContext, useCallback, useState } from 'react';
+import { baseUrl, postRequest } from '../utils/services';
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [registerError, setRegisterError] = useState(null);
+    const [isRegisterLoading, setIsRegisterLoading] = useState(false);
     const [registerInfo, setRegisterInfo] = useState({
         name: '',
         email: '',
         password: '',
     });
 
-    console.log('registerInfo', registerInfo);
-
     const updateRegisterInfo = useCallback((info) => {
         setRegisterInfo(info);
     }, []);
 
+    console.log(registerInfo);
+
+    const registerUser = useCallback(async (e) => {
+        e.preventDefault();
+        setIsRegisterLoading(true);
+        setRegisterError(null);
+
+        console.log(registerInfo);
+
+        const response = await postRequest(
+            `${baseUrl}/users/register`,
+            JSON.stringify(registerInfo)
+        );
+
+        setIsRegisterLoading(false);
+
+        if (response.error) {
+            return setRegisterError({ error: true, message: response.message });
+        }
+
+        localStorage.setItem('User', JSON.stringify(response));
+        setUser(response);
+    }, []);
+
     return (
         <AuthContext.Provider
-            value={{ user, registerInfo, updateRegisterInfo }}>
+            value={{
+                user,
+                registerInfo,
+                registerError,
+                isRegisterLoading,
+                updateRegisterInfo,
+                registerUser,
+            }}>
             {children}
         </AuthContext.Provider>
     );
