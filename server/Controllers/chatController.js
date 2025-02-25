f8c40fbc74bc95ab29e6444847c6ce5045543c60
Almas Yoko_office <Almas almas@yoko.space>
2025-02-25 13:04:44 +0500

    feat: implement chat functionality with create, findUserChats, and findChat endpoints

--- server/Controllers/chatController.js
+++ server/Controllers/chatController.js
@@ -1,24 +1,70 @@
 const chatModel = require('../Models/chatModel');
 
 // createChat
-// getUserChats
+// findUserChats
 // findChat
 
 const createChat = async (req, res) => {
     const { firstId, secondId } = req.body;
 
     try {
-        const chat = new chatModel({ members: [firstId, secondId] });
+        const chat = await chatModel.findOne({
+            members: { $all: [firstId, secondId] }
+        });
 
-        await chat.save();
+        if (chat) {
+            return res.status(200).json(chat);
+        }
 
-        res.status(200).json(chat);
+        const newChat = new chatModel({
+            members: [firstId, secondId]
+        });
 
+        const response = await newChat.save();
+
+        res.status(200).json(response);
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
     }
-}
+};
+
+const findUserChats = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const chats = await chatModel.find({
+            members: { $in: [userId] }
+        });
+
+        res.status(200).json(chats);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
+const findChat = async (req, res) => {
+    const { firstId, secondId } = req.params;
+
+    try {
+        const chat = await chatModel.find({
+            members: { $all: [firstId, secondId] }
+        });
+
+        res.status(200).json(chat);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
+
+module.exports = {
+    createChat,
+    findUserChats,
+    findChat
+};
 
 // 08:24
 // https://www.youtube.com/watch?v=LRQu77qJawU&list=PL63c_Ws9ecIRZ6njHRi3cuCkNSfzqyLBn&index=11&ab_channel=ChaooCharles
