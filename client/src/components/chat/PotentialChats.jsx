a3ea0e48aae0cb20aec8226a5288357beb0517ff
Almas Yoko_office <Almas almas@yoko.space>
2025-03-01 14:02:07 +0500

    feat: add PotentialChats component to display users available for new chats and implement chat creation functionality

---
+++ client/src/components/chat/PotentialChats.jsx
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import { ChatContext } from '../../context/ChatContext';
+import { AuthContext } from '../../context/AuthContext';
+
+const PotentialChats = () => {
+    const { user: authorizedUser } = useContext(AuthContext);
+    const { potentialChats, createChat } = useContext(ChatContext);
+    console.log('Potential Chats: ', potentialChats);
+    return (
+        <>
+            <div className='all-users'>
+                {potentialChats && potentialChats.length > 0 ? (
+                    potentialChats.map((user, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className='single-user'
+                                onClick={() =>
+                                    createChat(authorizedUser._id, user._id)
+                                }>
+                                {user.name}
+                                <span className='user-online'></span>
+                            </div>
+                        );
+                    })
+                ) : (
+                    <p>No potential chats</p>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default PotentialChats;
--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useCallback } from 'react';
 import { baseUrl, getRequest, postRequest } from '../utils/services';
 
 export const ChatContext = createContext();
@@ -7,6 +7,39 @@ export const ChatContextProvider = ({ children, user }) => {
     const [userChats, setUserChats] = useState(null);
     const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
     const [userChatsError, setUserChatsError] = useState(null);
+    const [potentialChats, setPotentialChats] = useState([]);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const response = await getRequest(`${baseUrl}/users`);
+
+            if (response.error) {
+                return console.error('Error fetching users', response.message);
+            }
+
+            const pChats = response.filter((u) => {
+                console.log('u: ', u);
+                let isChatCreated = false;
+                if (user?._id === u._id) return false;
+
+                if (userChats) {
+                    console.log('User Chats from getUsers: ', userChats);
+                    isChatCreated = userChats?.some((chat) => {
+                        return (
+                            chat?.members[0] === u._id ||
+                            chat?.members[1] === u._id
+                        );
+                    });
+                }
+
+                return isChatCreated ? false : true;
+            });
+
+            setPotentialChats(pChats);
+        };
+
+        getUsers();
+    }, [userChats]);
 
     useEffect(() => {
         const getUserChats = async () => {
@@ -26,13 +59,25 @@ export const ChatContextProvider = ({ children, user }) => {
                         message: response.message,
                     });
                 }
-
+                console.log('User Chats from getUserChats: ', response);
                 setUserChats(response);
             }
         };
 
         getUserChats();
     }, [user]);
+
+    const createChat = useCallback(async (firstId, secondId) => {
+        const response = await postRequest(
+            `${baseUrl}/chats`,
+            JSON.stringify({ firstId, secondId })
+        );
+        if (response.error) {
+            return console.error('Error creating chat', response.message);
+        }
+
+        setUserChats((prev) => [...prev, response]);
+    }, []);
 
     return (
         <ChatContext.Provider
@@ -43,6 +88,8 @@ export const ChatContextProvider = ({ children, user }) => {
                 setIsUserChatsLoading,
                 userChatsError,
                 setUserChatsError,
+                potentialChats,
+                createChat,
             }}>
             {children}
         </ChatContext.Provider>
