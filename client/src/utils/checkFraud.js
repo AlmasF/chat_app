ea48494c55aae4bd581a20ee7bbb59bb774bde04
Almas Yoko_office <Almas almas@yoko.space>
2026-03-08 21:34:38 +0500

    fix(checkFraud): Исправил билд

--- client/src/utils/checkFraud.js
+++ client/src/utils/checkFraud.js
@@ -6,16 +6,17 @@ env.allowRemoteModels = false;
 const FRAUD_THRESHOLD_STRICT = 0.85;
 const FRAUD_THRESHOLD_SOFT = 0.55;
 // Загружаем классификатор (путь к папке с моделью)
-const classifier = await pipeline("text-classification", "onnx_quantized", {
-  quantized: true,
-}).catch((e) => {
-  console.log(e);
-  console.log("env: ", env);
-});
+
+let instance = null;
 
 export const checkFraud = async (text) => {
   try {
-    const result = await classifier(text);
+    if (!instance) {
+      instance = await pipeline("text-classification", "onnx_quantized", {
+        quantized: true,
+      });
+    }
+    const result = await instance(text);
 
     const label = result?.length ? result[0].label : "";
     const score = result?.length ? result[0].score : NaN;
