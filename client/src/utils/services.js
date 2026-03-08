f878bf81463bd3a2de963179c268a376662fcdca
Almas Yoko_office <Almas almas@yoko.space>
2026-03-08 17:17:20 +0500

    feat(lock files): Обновил пакеты для использование через pnpm

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,47 +1,47 @@
 // export const baseUrl = "http://localhost:5000/api";
-export const baseUrl = "https://chat-app-1jpv.onrender.com/api";
+export const baseUrl = `${import.meta.env.VITE_SOCKET_URL}/api`;
 
 export const postRequest = async (url, body) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body,
-    });
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
+  });
 
-    const data = await response.json();
+  const data = await response.json();
 
-    if (!response.ok) {
-        let message = "An error occurred...";
+  if (!response.ok) {
+    let message = "An error occurred...";
 
-        if (data?.message) {
-            message = data.message;
-        } else {
-            message = data;
-        }
-
-        return { error: true, message };
+    if (data?.message) {
+      message = data.message;
+    } else {
+      message = data;
     }
 
-    return data;
+    return { error: true, message };
+  }
+
+  return data;
 };
 
 export const getRequest = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
-    if (!response.ok) {
-        let message = "An error occurred...";
+  if (!response.ok) {
+    let message = "An error occurred...";
 
-        if (data?.message) {
-            message = data.message;
-        } else {
-            message = data;
-        }
-
-        return { error: true, message };
+    if (data?.message) {
+      message = data.message;
+    } else {
+      message = data;
     }
 
-    return data;
+    return { error: true, message };
+  }
+
+  return data;
 };
