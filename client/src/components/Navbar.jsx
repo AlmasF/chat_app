05a6cc9b620a50421a316ec79373e6219c2d89f0
Almas Yoko_office <Almas almas@yoko.space>
2026-03-09 11:04:53 +0500

    feat: some translations and .env deletion

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,63 +1,56 @@
-import { useContext } from 'react';
-import { Container, Nav, Navbar, Stack } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext';
-import Notification from './chat/Notification';
+import { useContext } from "react";
+import { Container, Nav, Navbar, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import Notification from "./chat/Notification";
 
 const NavBar = () => {
-    const { user, logoutUser } = useContext(AuthContext);
+  const { user, logoutUser } = useContext(AuthContext);
 
-    return (
-        <Navbar
-            bg='dark'
-            className='mb-4'
-            style={{ height: '3.75rem' }}>
-            <Container>
-                <h2>
-                    <Link
-                        to='/'
-                        className='link-light text-decoration-none'>
-                        Chat App
-                    </Link>
-                </h2>
-                {user?.name && (
-                    <span className='text-warning'>
-                        Logged in as {user?.name}
-                    </span>
-                )}
-                <Nav>
-                    <Stack
-                        direction='horizontal'
-                        gap={2}>
-                        {user && (
-                            <>
-                                <Notification />
-                                <Link
-                                    onClick={() => logoutUser()}
-                                    className='link-light text-decoration-none'>
-                                    Logout
-                                </Link>
-                            </>
-                        )}
-                        {!user && (
-                            <>
-                                <Link
-                                    to='/login'
-                                    className='link-light text-decoration-none'>
-                                    Login
-                                </Link>
-                                <Link
-                                    to='/register'
-                                    className='link-light text-decoration-none'>
-                                    Register
-                                </Link>
-                            </>
-                        )}
-                    </Stack>
-                </Nav>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
+      <Container>
+        <h2>
+          <Link to="/" className="link-light text-decoration-none">
+            Chat App/Антифрод
+          </Link>
+        </h2>
+        {user?.name && (
+          <span className="text-warning">
+            Вошли как пользователь: {user?.name}
+          </span>
+        )}
+        <Nav>
+          <Stack direction="horizontal" gap={2}>
+            {user && (
+              <>
+                <Notification />
+                <Link
+                  onClick={() => logoutUser()}
+                  className="link-light text-decoration-none"
+                >
+                  Выйти из аккаунта
+                </Link>
+              </>
+            )}
+            {!user && (
+              <>
+                <Link to="/login" className="link-light text-decoration-none">
+                  Войти
+                </Link>
+                <Link
+                  to="/register"
+                  className="link-light text-decoration-none"
+                >
+                  Зарегистрироваться
+                </Link>
+              </>
+            )}
+          </Stack>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
